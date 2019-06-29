aba7873d2e6ec6e1c2db6a123c09669cdf232a5e
Patrick Harrison <jahman@bellatlantic.net>
2019-06-29 23:54:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-  ({array|index|})
+  scale = [2,3,4,5][2]
+find_element_index(scale,2)
 end
 
 def find_max_value(array)
-  # ([1,2,4,5,8,3,2,1])
+  
 end
 
 def find_min_value(array)
-  ([0,1,2,3,4,57,8,9,])
+  
 end
